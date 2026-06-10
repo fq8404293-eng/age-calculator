e9b343faf0b117717cd03ce14415199e20dbe7ed
fq8404293-eng <fq8404293@gmail.com>
2026-06-10 12:42:17 +0530

    Update percentage.js

--- percentage.js
+++ percentage.js
@@ -1,1 +1,16 @@
+function calculatePercentage() {
 
+    let percent = parseFloat(document.getElementById("percent").value);
+    let number = parseFloat(document.getElementById("number").value);
+
+    if (isNaN(percent) || isNaN(number)) {
+        document.getElementById("result").innerText =
+        "Please enter valid values.";
+        return;
+    }
+
+    let answer = (percent / 100) * number;
+
+    document.getElementById("result").innerText =
+    percent + "% of " + number + " = " + answer;
+}
