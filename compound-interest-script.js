e56b5e5e2fe872c6144684a28198df7a6c68fa4c
fq8404293-eng <fq8404293@gmail.com>
2026-06-11 15:33:39 +0530

    Create compound-interest-script.js

---
+++ compound-interest-script.js
@@ -0,0 +1,27 @@
+function calculateCompoundInterest() {
+
+    let principal = parseFloat(document.getElementById("principal").value);
+    let rate = parseFloat(document.getElementById("rate").value);
+    let years = parseFloat(document.getElementById("years").value);
+
+    if (
+        isNaN(principal) ||
+        isNaN(rate) ||
+        isNaN(years) ||
+        principal <= 0 ||
+        rate <= 0 ||
+        years <= 0
+    ) {
+        alert("Please enter valid values.");
+        return;
+    }
+
+    let futureValue = principal * Math.pow((1 + rate / 100), years);
+    let interestEarned = futureValue - principal;
+
+    document.getElementById("futureValue").innerText =
+        futureValue.toFixed(2);
+
+    document.getElementById("interestEarned").innerText =
+        interestEarned.toFixed(2);
+}
