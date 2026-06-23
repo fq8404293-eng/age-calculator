c6bd3776714d90c084a0297437a012a473da4684
fq8404293-eng <fq8404293@gmail.com>
2026-06-23 14:05:08 +0530

    Update tip-script.js

--- tip-script.js
+++ tip-script.js
@@ -1,7 +1,20 @@
+function setTip(percent) {
+    document.getElementById("tipPercent").value = percent;
+}
+
 function calculateTip() {
 
-    let billAmount = parseFloat(document.getElementById("billAmount").value);
-    let tipPercent = parseFloat(document.getElementById("tipPercent").value);
+    let billAmount = parseFloat(
+        document.getElementById("billAmount").value
+    );
+
+    let tipPercent = parseFloat(
+        document.getElementById("tipPercent").value
+    );
+
+    let people = parseInt(
+        document.getElementById("people").value
+    );
 
     if (
         isNaN(billAmount) ||
@@ -13,12 +26,22 @@ function calculateTip() {
         return;
     }
 
+    if (isNaN(people) || people < 1) {
+        people = 1;
+    }
+
     let tipAmount = billAmount * (tipPercent / 100);
+
     let totalBill = billAmount + tipAmount;
+
+    let perPerson = totalBill / people;
 
     document.getElementById("tip").innerText =
         tipAmount.toFixed(2);
 
     document.getElementById("total").innerText =
         totalBill.toFixed(2);
+
+    document.getElementById("perPerson").innerText =
+        perPerson.toFixed(2);
 }
